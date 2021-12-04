b1872653b0a3b6b8bbb22df27be98e2ffcf0ee0a
gitccl <chenchuanle6@gmail.com>
2021-12-04 21:38:24 +0800

    ready to submit my lab1

--- user/xargs.c
+++ user/xargs.c
@@ -5,9 +5,17 @@
 #include "kernel/fs.h"
 
 #define MAXLENGTH 2000
-char buf[MAXLENGTH];
-char *exec_argv[MAXARG]; 
 
+typedef struct {
+  char str[MAXLENGTH];
+  int len, last_pos;
+}buf_t;
+
+typedef struct {
+  char *argv[MAXARG]; 
+  int argc, remains;
+  int exec_argc_init, n, is_ever_exec;
+}parse_t;
 
 int is_whilespace(char ch) {
   return ch == ' ' || ch == '\t' || ch == '\n';
@@ -23,9 +31,9 @@ void skip(char *str, int *pos, int op) {
   }
 }
 
-void exec_wrap(int argc, char *argv[])
+void exec_wrap(parse_t *parse)
 {
-  argv[argc] = 0;
+  parse->argv[parse->argc] = 0;
   // for(int i = 0; argv[i]; ++ i) {
   //   printf("args%d: %s\n", i, argv[i]);
   // }
@@ -34,23 +42,53 @@ void exec_wrap(int argc, char *argv[])
     fprintf(2, "xargs: fork failed\n");
     exit(1);
   } else if(pid == 0){
-    exec(exec_argv[0], exec_argv);
+    exec(parse->argv[0], parse->argv);
   } else {
     wait(0);
   }
 }
 
+parse_t parse;
+buf_t buf;
+
+void solve(buf_t *buf, parse_t *parse)
+{
+  while (1)
+  {
+    while(parse->remains > 0 && buf->last_pos < buf->len) {
+      skip(buf->str, &buf->last_pos, 0);
+      if (parse->argc >= MAXARG) {
+        fprintf(2, "xargs: too many args, exceed 32\n");
+        exit(1);
+      }
+      if(buf->last_pos >= buf->len) break;
+      parse->argv[parse->argc ++] = buf->str + buf->last_pos;
+      skip(buf->str, &buf->last_pos, 1);
+      buf->str[buf->last_pos ++] = '\0';
+      parse->remains --;
+    }
+    if (parse->remains > 0) break;
+    exec_wrap(parse);
+    parse->remains = parse->n;
+    parse->argc = parse->exec_argc_init;
+    parse->is_ever_exec = 1;
+  }
+} 
+
 int
 main(int argc, char *argv[])
 {
-  int has_n = 0, n = MAXARG, cmd_start = 1;
+  int has_n = 0, cmd_start = 1;
+  parse.n = MAXARG;
   if (argc >= 3) {
     if(strcmp(argv[1], "-n") == 0) {
       has_n = 1;
-      n = atoi(argv[2]);
+      parse.n = atoi(argv[2]);
       cmd_start = 3;
     }
   }
+
+  char **exec_argv = parse.argv;
 
   exec_argv[0] = "echo"; // default to `echo`
   for(int i = cmd_start; i < argc; ++ i) {
@@ -60,85 +98,46 @@ main(int argc, char *argv[])
     }
     exec_argv[i - cmd_start] = argv[i];
   }
-  int exec_argc_init = argc > cmd_start ? argc - cmd_start : 1;
 
-  int len = 0, last_pos = 0;
-  int num = n, is_done = 0;
-  int exec_argc = exec_argc_init;
+  parse.exec_argc_init = argc > cmd_start ? argc - cmd_start : 1;
+  parse.remains = parse.n;
+  parse.is_ever_exec = 0;
+
+  parse.argc = parse.exec_argc_init;
   int special_ch = 0;
-  while (read(0, buf + len, 1))
+  while (read(0, buf.str + buf.len, 1))
   {
-    char ch = buf[len];
+    char ch = buf.str[buf.len];
     if(ch == '\"') continue;
     if(ch == '\\') {
       if(!special_ch) special_ch = 1;
       else {
         special_ch = 0;
-        buf[len] = '\\';
+        buf.str[buf.len] = '\\';
       }
     }
     if(ch == 'n' && special_ch) {
       special_ch = 0;
-      buf[len] = ch = '\n';
+      buf.str[buf.len] = ch = '\n';
     }
     if(special_ch) continue;
-    ++ len;
-    if (len >= MAXLENGTH) {
+    ++ buf.len;
+    if (buf.len >= MAXLENGTH) {
       fprintf(2, "input too long!\n");
       exit(1);
     }
 
     if(ch == '\n' && has_n) {
-      while (1)
-      {
-        while(num > 0 && last_pos < len) {
-          is_done = 0;
-          skip(buf, &last_pos, 0);
-          if (exec_argc >= MAXARG) {
-            fprintf(2, "xargs: too many args, exceed 32\n");
-            exit(1);
-          }
-          if(last_pos >= len) break;
-          exec_argv[exec_argc ++] = buf + last_pos;
-          skip(buf, &last_pos, 1);
-          buf[last_pos ++] = '\0';
-          num --;
-        }
-        if (num > 0) break;
-        // exec
-        exec_wrap(exec_argc, exec_argv);
-        num = n;
-        exec_argc = exec_argc_init;
-        is_done = 1;
-      }
+      solve(&buf, &parse);
     }
   }
   
-  // merge has -n and hasn't -n
-  if (exec_argc == exec_argc_init && is_done) {
+  if (parse.argc == parse.exec_argc_init && parse.is_ever_exec) {
     exit(0);
   }
-  while (1)
-  {
-    while(num > 0 && last_pos < len) {
-      skip(buf, &last_pos, 0);
-      if (exec_argc >= MAXARG) {
-        fprintf(2, "xargs: too many args, exceed 32\n");
-        exit(1);
-      }
-      if(last_pos >= len) break;
-      exec_argv[exec_argc ++] = buf + last_pos;
-      skip(buf, &last_pos, 1);
-      buf[last_pos ++] = '\0';
-      num --;
-    }
-    if (num > 0) break;
-    exec_wrap(exec_argc, exec_argv);
-    num = n;
-    exec_argc = exec_argc_init;
-  }
-  if(exec_argc > 1) {
-    exec_wrap(exec_argc, exec_argv);
+  solve(&buf, &parse);
+  if(parse.argc > 1) {
+    exec_wrap(&parse);
   }
   exit(0);
 }
